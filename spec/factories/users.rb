e700ce17d79c3ea5097aee72991765bdf60a58d8
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2021-10-14 01:06:37 +0700

    Add some test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :user do
-    first_name { 'MyString' }
-    last_name { 'MyString' }
-    age { 1 }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    age { Faker::Number.between(from: 18, to: 35) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:someone) { create(:user) }
+
+  describe '#like' do
+    it 'should create new Action record' do
+      expect { user.like(someone) }.to change { Action.all.size }.by 1
+    end
+
+    it 'should put someone to favoriting list' do
+      user.like(someone)
+      expect(user.favoriting).to include(someone)
+    end
+  end
+
+  describe '#pass' do
+    it 'should create new Action record' do
+      expect { user.pass(someone) }.to change { Action.all.size }.by 1
+    end
+
+    it 'should put someone to passing list' do
+      user.pass(someone)
+      expect(user.passing).to include(someone)
+    end
+  end
 end
