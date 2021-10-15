d14caf8a2ff029bea418829e92175dcc2b7945fd
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2021-10-15 23:00:20 +0700

    Feature/frontend connect api (#22)
    * Add axios library, upadte gitignore, clear ignore cached
    
    * Add cors to fix cross origin error
    
    * Fix error loading data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
-          {user}
+          {JSON.stringify(user)}
         </p>
         <a
           className="App-link"
