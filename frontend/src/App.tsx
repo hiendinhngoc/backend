95ca996a9849f0bb17629a8e0c9412f4675a2ba4
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2021-10-15 22:35:43 +0700

    Add axios library, upadte gitignore, clear ignore cached (#21)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const showUserInfo = () => {
+    axios.get('http://localhost:3001/api/v1/users')
+    .then((response: any) => {
+      setUser(response.data);
+    }).catch(error => console.log("calling api error ", error))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          {user}
         </p>
         <a
           className="App-link"
@@ -18,6 +31,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={showUserInfo}>Show User</button>
       </header>
     </div>
   );
