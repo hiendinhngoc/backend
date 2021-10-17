fd5472a336847a2e793a22f9a0f8c45cb396a53b
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2021-10-17 23:57:17 +0700

    Add favoriting page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Discovery from './components/Discovery';
+import Favoriting from './components/Favoriting';
 import './App.css';
 
 interface User {
@@ -14,6 +15,9 @@ interface User {
 function App() {
   const [initialUsers, setInitialUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
+  const [isDiscovery, setIsDiscovery] = useState(true);
+  const [isFavoriting, setIsFavoriting] = useState(false);
+  // const [isMatching, setIsMatching] = useState(false);
 
   const getUsers = async () => {
     await axios.get<User[]>('http://localhost:3001/api/v1/users')
@@ -27,11 +31,27 @@ function App() {
     getUsers()
   }, []);
 
+  const handleOpenDiscovery = () => {
+    setIsDiscovery(true);
+    setIsFavoriting(false);
+  }
+
+  const handleOpenFavoriting = () => {
+    setIsDiscovery(false);
+    setIsFavoriting(true);
+  }
+
   if(loading) { return <div>Loading...</div>}
 
   return (
     <div className="app">
-      <Discovery initialUsers={initialUsers} />
+      {isDiscovery && <Discovery initialUsers={initialUsers} />}
+      {isFavoriting && <Favoriting />}
+      <div className="buttons">
+        <button onClick={handleOpenFavoriting}>Favoriting</button>
+        <button onClick={handleOpenDiscovery}>Discover</button>
+        <button onClick={undefined}>Matches</button>
+      </div>
     </div>
   );
 }
