1ff62ae70c97c60770b8cbf50f7b2163267d1759
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2021-10-17 23:58:11 +0700

    Add favoriting page (#24)

---
+++ frontend/src/components/Favoriting.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './favoriting_style.css'
+
+interface User {
+  id: number,
+  first_name: string,
+  last_name: string,
+  age: number,
+  image: string,
+}
+
+const Favoriting: React.FC = () => {
+  const [favoriting, setFavoriting] = useState<User[]>([])
+
+  const getFavoritings = async () => {
+    await axios.get<User[]>('http://localhost:3001/api/v1/user/favoriting')
+    .then((response) => {
+      setFavoriting(response.data);
+    }).catch(error => console.log("calling get list favoriting error ", error))
+  }
+
+  useEffect(() => {
+    getFavoritings()
+  }, []);
+
+  return (
+    <div className="grid-container">
+      { 
+        favoriting.map((user, index) =>
+          <div key={index} className="grid-item" >
+            <div style={{ backgroundImage: 'url('+ user.image +')' }} className="card-favoriting">
+              <h3>{user.first_name} {user.last_name}, {user.age}</h3>
+            </div>
+          </div>
+        )
+      }
+    </div>
+  );
+}
+
+export default Favoriting;
