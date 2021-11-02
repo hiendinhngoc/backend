9acf00775367e350d85a4a322ee9bfb625e52909
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2021-11-03 00:56:55 +0700

    Refactor the migration file

--- backend/db/migrate/20211031151523_update_passive_action_foreign_key_to_action.rb
+++ backend/db/migrate/20211031151523_update_passive_action_foreign_key_to_action.rb
@@ -1,14 +1,9 @@
 class UpdatePassiveActionForeignKeyToAction < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :actions, :owner_id, :bigint 
 
-    Action.all.each do |action|
-      if action.like_id.present?
-        action.update owner_id: action.like_id
-      elsif 
-        action.update owner_id: action.pass_id
-      end
-    end
+    Action.where.not(like_id: nil).update_all('owner_id = like_id')
+    Action.where.not(pass_id: nil).update_all('owner_id = pass_id')
 
     remove_index :actions, :like_id
     remove_index :actions, %i[liked_id like_id], unique: true
@@ -18,5 +13,21 @@ class UpdatePassiveActionForeignKeyToAction < ActiveRecord::Migration[6.1]
 
     add_index :actions, :owner_id
     add_index :actions, %i[owner_id liked_id], unique: true  
+  end
+
+  def down
+    remove_index :actions, %i[owner_id liked_id], unique: true
+    remove_index :actions, :owner_id
+
+    add_column :actions, :like_id, :integer
+    add_column :actions, :pass_id, :integer
+
+    add_index :actions, %i[liked_id like_id], unique: true
+    add_index :actions, :like_id
+
+    Action.where.not(passed_id: nil).update_all('pass_id = owner_id')
+    Action.where.not(liked_id: nil).update_all('like_id = owner_id')
+
+    remove_column :actions, :owner_id
   end
 end
